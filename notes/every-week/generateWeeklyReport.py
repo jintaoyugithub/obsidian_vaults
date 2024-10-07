3d5f7baf9dacfbb977ec1bb3a8fd94da244a9920
clay <clay_codes1024@163.com>
2024-10-08 00:26:25 +0200

    finish scripts for generating the table for weekly report

--- notes/every-week/generateWeeklyReport.py
+++ notes/every-week/generateWeeklyReport.py
@@ -1,7 +1,7 @@
 import re
 import glob
 import argparse
-from py_markdown_table import MarkdownTable
+from py_markdown_table.markdown_table import markdown_table
 
 '''
 The extracted data should looks like:
@@ -11,10 +11,11 @@ The extracted data should looks like:
         ...
 ]
 '''
+def CreateMDTable(filepaths):
+    # Open and read the file
+    data = []
 
-def ExtractTableFromFile(filepath):
-    try:    
-        # Open and read the file
+    for filepath in filepaths:
         with open(filepath, 'r') as file:
             md_content = file.read()
 
@@ -25,14 +26,63 @@ def ExtractTableFromFile(filepath):
         headers = re.findall(find_headers, md_content)
         contents = re.findall(find_contents, md_content)
 
-        print(headers)
-        print(contents)
+        # make sure the contents are not empty
+        if contents:
+            # extract the data from the header
+            Top, Branch, Time = headers
+            # construct the table with the data
+            for row in contents:
+                top, branch, time = row
+                time = int(time)
+                # check if the branch is already exist
+                found = False
+                for item in data:
+                    if item[Branch] == branch:
+                        item[Time] += time
+                        found= True
+                        break
+
+                if not found:
+                    data.append({
+                        Top: top,
+                        Branch: branch,
+                        Time: time
+                })
+        else:
+            print("empty contents")
+
+    # create the markdown table
+    markdown = markdown_table(data).get_markdown()
+    print(markdown)
 
 
-    except Exception as e:
-        print("FATAL ERROR: {e}")
+def GenerateFilePath():
+    parser = argparse.ArgumentParser(description="Process files input")
+    parser.add_argument('files', nargs='+', help='Process markdown md files')
+    args = parser.parse_args()
 
-ExtractTableFromFile('/Users/jintao/vaults/notes/daily-notes/2024-10-03.md')
+    all_files = []
+    for pattern in args.files:
+        all_files.extend(glob.glob(pattern))
+
+    # 输出匹配的文件
+    if all_files:
+        root = './'
+        for i in range(len(all_files)):
+            all_files[i] = root + all_files[i]
+            print("Matched file: " + all_files[i])
+        return all_files
+    else:
+        print("No files matched.")
+
+
+def Run():
+    files = GenerateFilePath()
+    CreateMDTable(files)
+
+
+if __name__ == "__main__":
+    Run()
 
 
 
